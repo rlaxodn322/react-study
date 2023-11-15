4470c9518c4570e709760f19c1f3453762f34bb6
taewoo <rlaxodn322@gmail.com>
2023-11-15 21:01:48 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Container, Nav, Navbar, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import data from "./data";
 function App() {
+  let [shoes, setShoes] = useState({ data });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="row">
+        <Card shoes={shoes} i={1} />
+        <Card shoes={shoes} i={2} />
+        <Card shoes={shoes} i={3} />
+      </div>
     </div>
   );
 }
-
+const Card = (props) => {
+  return (
+    <div className="col-md-4">
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+    </div>
+  );
+};
 export default App;
